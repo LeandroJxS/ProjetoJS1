3ab14a7355038739c72a8d2a230327fdee34c169
LeandroJxS <Leandro22.JS@gmail.com>
2022-01-20 13:12:17 -0300

    Teste 3

--- Script.js
+++ Script.js
@@ -1,23 +1,54 @@
 class Produto {
 
     constructor() {
-        this.id = 0;
-        this.nomeProduto = '';
-        this.valor = 0;
+        this.id = 1;
+        this.arrayProdutos = [];
+       
+    }
+
+    salvar() {
+        let produto = this.lerDados();
+
+        if(this.validaCampos(produto)) {
+            alert('salvar')
+        }
+
+        console.log(produto);
+
+    }
+
+    lerDados() {
+        let produto = {}
+
+        produto.id = this.id;
+        produto.nomeProduto = document.getElementById('produto').value;
+        produto.valor = document.getElementById('valor').value;
     
+        return produto;
     }
 
-    adicionar() {
-        alert('Vamos adicionar um produto!');
-        // ...
+    validaCampos(produto) {
+        let msg = '';
+
+        if(produto.nomeProduto == '') {
+            msg += '- Informe o nome do Produto! \n'
+        }
+
+        if(produto.valor == '') {
+            msg += '- Informe o Valor! \n'
+        }
+
+        if(msg != '') {
+            alert(msg);
+            return false
+        }
+
+        return true;
+
+    cancelar() {
+        
     }
-
-    excluir() {
-
-        // ...
-
-        alert('Item deletado!');
-    }
+    
 }
 
 var produto = new Produto();
